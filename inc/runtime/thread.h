093cdc05d68d1f8095ab64596e0aad317233caba
Josh Fried <joshuafried@gmail.com>
2023-02-19 00:34:47 -0500

    add space for Thread class in struct thread

--- inc/runtime/thread.h
+++ inc/runtime/thread.h
@@ -40,6 +40,7 @@ struct thread {
 	struct list_node	interruptible_link;
 	uint64_t	tlsvar;
 	uint64_t	fsbase;
+	unsigned long		junction_tstate_buf[8];
 };
 
 extern uint64_t thread_get_total_cycles(thread_t *th);
