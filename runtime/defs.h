250f782d3c8e36bf9b5c292a7f904f52abce409a
Nanqinqin Li <linanqinqin@princeton.edu>
2025-08-13 18:01:13 -0400

    nit

--- runtime/defs.h
+++ runtime/defs.h
@@ -437,11 +437,11 @@ extern unsigned int lame_bundle_get_used_count(struct kthread *k);
 /* LAME scheduling functions */
 extern thread_t *lame_sched_get_next_uthread(struct kthread *k);
 extern thread_t *lame_sched_get_current_uthread(struct kthread *k);
-extern bool lame_sched_is_enabled(struct kthread *k);
 
 /* Dynamic bundle scheduling control functions */
 extern void lame_sched_enable(struct kthread *k);
 extern void lame_sched_disable(struct kthread *k);
+extern bool lame_sched_is_enabled(struct kthread *k);
 extern bool lame_sched_is_statically_enabled(struct kthread *k);
 extern bool lame_sched_is_dynamically_enabled(struct kthread *k);
 
