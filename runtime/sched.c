8ba788597fcf26483891bf4bcfba98262b9e1130
Nanqinqin Li <linanqinqin@princeton.edu>
2025-08-13 23:27:22 -0400

    [runtime/sched] use log_info

--- runtime/sched.c
+++ runtime/sched.c
@@ -85,7 +85,7 @@ static void jmp_thread(thread_t *th)
 
 	/* linanqinqin */
 	/* LAME: Log when uthread is started (scheduled on kthread) */
-	log_debug("[LAME][sched ON]: uthread %p started (scheduled on kthread %d)", 
+	log_info("[LAME][sched ON]: uthread %p started (scheduled on kthread %d)", 
 		  th, kthread_idx(myk()));
 	/* end */
 
@@ -125,7 +125,7 @@ static void jmp_thread_direct(thread_t *oldth, thread_t *newth)
 
 	/* linanqinqin */
 	/* LAME: Log when uthread is switched directly (oldth descheduled, newth scheduled) */
-	log_debug("[LAME][sched ON]: direct switch: uthread %p scheduled on kthread %d", 
+	log_info("[LAME][sched ON]: direct switch: uthread %p scheduled on kthread %d", 
 		  newth, kthread_idx(myk()));
 	/* end */	
 
@@ -363,7 +363,7 @@ static __noinline void schedule(void)
 	if (likely(th != NULL)) {
 		/* linanqinqin */
 		/* LAME: Log when uthread is descheduled from kthread */
-		log_debug("[LAME][sched OFF]: uthread %p descheduled from kthread %d", 
+		log_info("[LAME][sched OFF]: uthread %p descheduled from kthread %d", 
 			  th, kthread_idx(l));
 		/* end */
 		
@@ -506,7 +506,7 @@ static __always_inline void enter_schedule(thread_t *curth)
 
 	/* linanqinqin */
 	/* LAME: Log when uthread enters scheduler (descheduled from kthread) */
-	log_debug("[LAME][sched OFF]: uthread %p entering scheduler (descheduled from kthread %d)", 
+	log_info("[LAME][sched OFF]: uthread %p entering scheduler (descheduled from kthread %d)", 
 		  curth, kthread_idx(k));
 	/* end */	
 
@@ -780,7 +780,7 @@ void thread_finish_yield(void)
 
 	/* linanqinqin */
 	/* LAME: Log when uthread cedes (descheduled from kthread) */
-	log_debug("[LAME][sched OFF]: uthread %p ceding (descheduled from kthread %d)", 
+	log_info("[LAME][sched OFF]: uthread %p ceding (descheduled from kthread %d)", 
 		  curth, kthread_idx(k));
 	/* end */
 
@@ -799,7 +799,7 @@ void thread_finish_cede(void)
 
 	/* linanqinqin */
 	/* LAME: Log when uthread cedes (descheduled from kthread) */
-	log_debug("[LAME][sched OFF]: uthread %p ceding (descheduled from kthread %d)", 
+	log_info("[LAME][sched OFF]: uthread %p ceding (descheduled from kthread %d)", 
 		  myth, kthread_idx(k));
 	/* end */
 
