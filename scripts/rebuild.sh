bd41b1023a38b27c91997161b1eff1f67fe0ca22
Nanqinqin Li <linanqinqin@princeton.edu>
2025-10-10 00:26:59 -0400

    [scripts] add rebuild

---
+++ scripts/rebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# exit immediately if any command fails
+set -e  
+
+# go to the home directory of caladan-lame
+cd "$(dirname "$0")/.."
+CALADAN_HOME=$(pwd)
+
+# rebuild caladan
+make clean
+make -j
+
+# rebuild ksched
+cd ksched/
+make clean
+make
+
+# rebuild hello_world
+cd ../apps/hello_world
+make clean
+make threads matmul
+
+# setup machine and start iokerneld
+cd "$CALADAN_HOME"
+sudo ./scripts/setup_machine.sh
+sudo ./iokerneld
