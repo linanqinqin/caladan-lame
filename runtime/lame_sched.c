6dfce3e9229f351ef405dac897e63a922f79334d
Nanqinqin Li <linanqinqin@princeton.edu>
2025-10-21 21:01:39 -0400

    [runtime/lame_sched] parenthesis matter!

--- runtime/lame_sched.c
+++ runtime/lame_sched.c
@@ -251,7 +251,7 @@ __always_inline __nofp static thread_t *lame_sched_get_next_uthread(struct kthre
 __always_inline __nofp static thread_t *lame_sched_get_next_idx_uthread(struct kthread *k)
 {
 	struct lame_bundle *bundle = &k->lame_bundle;
-	return bundle->uthreads[(++bundle->active) % bundle->used].uthread;
+	return bundle->uthreads[(++(bundle->active)) % bundle->used].uthread;
 }
 
 /**
